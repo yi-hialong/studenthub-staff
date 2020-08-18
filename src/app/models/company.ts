29ca8294c98dd71a0e27cb3c53a0705a69e93302
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-08-18 16:40:15 +0530

    [Finishes #174360097,#174360157,#174362158] admin can now add/update some extra fields of company. staff can also view them same as admin

--- src/app/models/company.ts
+++ src/app/models/company.ts
@@ -4,6 +4,11 @@ export class Company{
     company_id: number;
     parent_company_id: number;
     company_name: string;
+    company_common_name_en: string;
+    company_common_name_ar: string;
+    company_description_en: string;
+    company_description_ar: string;
+    company_website: string;
     company_email: string;
     company_status: number;
     total_candidates: number;

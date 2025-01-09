3de9acc89212cce9165f8507f31f416edeba99a6
Er. Krushn Kathrecha <10800870+krushn@users.noreply.github.com>
2025-01-09 13:14:29 +0530

    part of wtf-415:  story view showing interest from other

--- src/app/pages/logged-in/story/story-view/story-view.page.ts
+++ src/app/pages/logged-in/story/story-view/story-view.page.ts
@@ -381,7 +381,10 @@ export class StoryViewPage implements OnInit, OnDestroy {
       this.loadStoryInvitations();
       this.loadSuggestions();
       this.loadNotes();
-      this.listInterests();
+
+      if (this.story.job) {
+        this.listInterests();
+      }
 
       if (this.story.story_status == 1 && this.story.staff_id == this.authService.staff_id &&
         ['cancelled', 'delivered'].indexOf(this.story.request.request_status) == -1)
